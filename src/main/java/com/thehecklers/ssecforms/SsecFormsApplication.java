65f6d080532570789742c9ce7404a37b6ff35d66
Mark Heckler <mark.heckler@gmail.com>
2019-02-03 23:28:42 +0100

    Added httpBasic() auth for CLI/remoting protocols

--- src/main/java/com/thehecklers/ssecforms/SsecFormsApplication.java
+++ src/main/java/com/thehecklers/ssecforms/SsecFormsApplication.java
@@ -55,7 +55,9 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 }
 
